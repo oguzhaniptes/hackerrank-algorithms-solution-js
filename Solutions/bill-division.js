2bc4f127f19b7e390f8e9d7b4d24e5dbe46d7187
Oğuzhan İpteş <53427115+oguzhaniptes@users.noreply.github.com>
2023-02-02 11:14:51 +0300

    bill division solved

--- Solutions/bill-division.js
+++ Solutions/bill-division.js
@@ -1,6 +1,5 @@
 function bonAppetit(bill, k, b) {
-    let Anna = new Array()
-    bill.splice(k, 1, ...Anna)
+    bill.splice(k, 1)
     let totalBill = bill.reduce((acc, cur) => acc + cur)
     let costPerPerson = totalBill / 2
     if (costPerPerson == b) {
